e548344be5976f3274affae53cb7459f216cefc5
bcrenato <bcrenato@gmail.com>
2025-07-19 14:50:39 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,33 @@
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+const CACHE_NAME = 'pwa-teste-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
+];
 
-firebase.initializeApp({
-  apiKey: "AIzaSyBC73aRe7HLp-zQNpJcbWLlUj24kiQGAcE",
-  authDomain: "cadastro-membros-c5cd4.firebaseapp.com",
-  databaseURL: "https://cadastro-membros-c5cd4-default-rtdb.firebaseio.com",
-  projectId: "cadastro-membros-c5cd4",
-  storageBucket: "cadastro-membros-c5cd4.appspot.com",
-  messagingSenderId: "250346042791",
-  appId: "1:250346042791:web:6bc469b844de69e526b282"
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-const messaging = firebase.messaging();
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
 
-messaging.onBackgroundMessage(payload => {
-  console.log('[service-worker.js] Mensagem recebida no background ', payload);
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/icons/icon-192x192.png',
-    image: payload.notification.image || undefined
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
 });
