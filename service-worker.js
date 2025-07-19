e8352401803b72e00f22d595d14832dba498d642
bcrenato <bcrenato@gmail.com>
2025-07-19 15:40:24 -0300

    Update and rename firebase-messaging-sw.js to service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,13 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBC73aRe7HLp-zQNpJcbWLlUj24kiQGAcE",
+   apiKey: "AIzaSyBC73aRe7HLp-zQNpJcbWLlUj24kiQGAcE",
   authDomain: "cadastro-membros-c5cd4.firebaseapp.com",
   databaseURL: "https://cadastro-membros-c5cd4-default-rtdb.firebaseio.com",
   projectId: "cadastro-membros-c5cd4",
-  storageBucket: "cadastro-membros-c5cd4.appspot.com", // <- corrigido aqui
+  storageBucket: "cadastro-membros-c5cd4.firebasestorage.app",
   messagingSenderId: "250346042791",
-  appId: "1:250346042791:web:6bc469b844de69e526b282",
+  appId: "1:250346042791:web:6bc469b844de69e526b282"
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -16,7 +16,7 @@ firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(payload => {
-  console.log('[service-worker.js] Mensagem recebida no background ', payload);
+  console.log('[service-worker.js] 📩 Mensagem recebida em background:', payload);
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
