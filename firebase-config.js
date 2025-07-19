1c206b0e18eb31260bba41dc8c9bf5b3d3585629
Renato <bcrenato@gmail.com>
2025-07-18 21:55:31 -0300

    Sua mensagem

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,9 @@
-// Configuração Firebase - substitua com seus dados do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBC73aRe7HLp-zQNpJcbWLlUj24kiQGAcE",
   authDomain: "cadastro-membros-c5cd4.firebaseapp.com",
   databaseURL: "https://cadastro-membros-c5cd4-default-rtdb.firebaseio.com",
   projectId: "cadastro-membros-c5cd4",
-  storageBucket: "cadastro-membros-c5cd4.firebasestorage.app",
+  storageBucket: "cadastro-membros-c5cd4.appspot.com", // <- corrigido aqui
   messagingSenderId: "250346042791",
   appId: "1:250346042791:web:6bc469b844de69e526b282",
 };
--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,9 @@
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
-
 const firebaseConfig = {
   apiKey: "AIzaSyBC73aRe7HLp-zQNpJcbWLlUj24kiQGAcE",
   authDomain: "cadastro-membros-c5cd4.firebaseapp.com",
   databaseURL: "https://cadastro-membros-c5cd4-default-rtdb.firebaseio.com",
   projectId: "cadastro-membros-c5cd4",
-  storageBucket: "cadastro-membros-c5cd4.firebasestorage.app",
+  storageBucket: "cadastro-membros-c5cd4.appspot.com", // <- corrigido
   messagingSenderId: "250346042791",
   appId: "1:250346042791:web:6bc469b844de69e526b282"
 };
